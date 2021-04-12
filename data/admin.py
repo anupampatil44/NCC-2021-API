f79b4175053e27b4da28478b166eb0e351cbb35f
anupampatil44 <anupampatil44@gmail.com>
2021-04-12 23:18:38 +0530

    registered models in admin.py

--- data/admin.py
+++ data/admin.py
@@ -3,5 +3,8 @@ from data.models import Userdata
 from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+from data.models import Question,Submission
 
 admin.site.register(Userdata)
+admin.site.register(Question)
+admin.site.register(Submission)
