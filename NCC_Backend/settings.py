d4511b5820daf5a506069adfcb2a3c978be2ae62
Anupam Patil <61505546+anupampatil44@users.noreply.github.com>
2021-09-07 10:58:51 +0530

    Update settings.py

--- NCC_Backend/settings.py
+++ NCC_Backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2(0bk&7l6rfvv_z^kvq4+^tmt4q+v&+ebsxrl*_tj2@-8=4yrs'
+SECRET_KEY = 'itsasecret'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
